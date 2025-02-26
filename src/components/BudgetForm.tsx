b2d4f0a3c301963881d25e2415fe3d59e9bbf59d
Jhofran Briceño <jhofran.dev@outlook.com>
2025-02-26 11:49:03 -0500

    se agrego el formulario

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,3 +1,37 @@
+import { useState, ChangeEvent } from "react";
+
 export default function BudgetForm() {
-  return <div>BudgetForm</div>;
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber);
+  };
+
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir Presupuesto
+        </label>
+        <input
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Definir tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+
+        <input
+          type="submit"
+          value="Definir Presupuesto"
+          className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+        />
+      </div>
+    </form>
+  );
 }
