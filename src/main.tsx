833c33765df9c9dc6d1c55c8b39a1c9600a2b771
Jhofran Briceño <jhofran.dev@outlook.com>
2025-03-13 08:31:34 -0500

    Agregando contextAPI

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BudgetProvider } from "./context/BudgetContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
   </StrictMode>
 );
-
-// Probando
