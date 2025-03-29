32d4b0ee968e3c9a8759ca58d97cce73f9a2fc08
Jhofran Briceño <jhofran.dev@outlook.com>
2025-03-29 18:02:42 -0500

    feat: add modal functionality and ExpenseForm component
    This commit introduces a modal feature to the budget application, allowing users to open and close a modal dialog. The modal is integrated with the budget reducer, which now includes actions for managing the modal state. Additionally, a new ExpenseForm component is created and rendered within the modal. These changes enhance the user experience by providing a structured way to input expenses.

--- src/components/ExpenseModal.tsx
+++ src/components/ExpenseModal.tsx
@@ -1,18 +1,26 @@
 import { Fragment } from "react";
-import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { Dialog, Transition } from "@headlessui/react";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { useBudget } from "../hooks/useBudget";
+import ExpenseForm from "./ExpenseForm";
 
 export default function ExpenseModal() {
+  const { state, dispatch } = useBudget();
+
   return (
     <>
       <div className="fixed right-5 bottom-5 flex items-center justify-center">
-        <button type="button">
+        <button type="button" onClick={() => dispatch({ type: "show-modal" })}>
           <PlusCircleIcon className="w-16 h-16 text-blue-600 rounded-full" />
         </button>
       </div>
 
-      <Transition appear show={false} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => {}}>
+      <Transition appear show={state.modal} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => dispatch({ type: "close-modal" })}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -36,7 +44,9 @@ export default function ExpenseModal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"></Dialog.Panel>
+                <Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <ExpenseForm />
+                </Dialog.Panel>
               </Transition.Child>
             </div>
           </div>
