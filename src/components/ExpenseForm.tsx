23e4e194071f0d9c422499fa83b85549514140da
Jhofran Briceño <jhofran.dev@outlook.com>
2025-03-30 13:30:31 -0500

    feat(ExpenseForm): add date picker and category dropdown
    Introduce `react-date-picker` and `react-calendar` dependencies to enable date selection in the ExpenseForm component. Additionally, add a category dropdown populated with predefined categories from the new `categories.ts` file. This enhances the form's functionality for better user input management.

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,3 +1,72 @@
+import DatePicker from "react-date-picker";
+import { categories } from "../data/categories";
+
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 export default function ExpenseForm() {
-  return <div>ExpenseForm</div>;
+  return (
+    <form>
+      <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
+        Nuevo Gasto
+      </legend>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre Gasto:
+        </label>
+        <input
+          type="text"
+          id="expenseName"
+          placeholder="Añade el Nombre del gasto"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Cantidad:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Añade la cantidad del gasto"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="category" className="text-xl">
+          Categoria:
+        </label>
+        <select id="category" className="bg-slate-100 p-2" name="category">
+          <option value="">-- Seleccione --</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+
+        <div className="flex flex-col gap-2">
+          <label htmlFor="" className="text-xl">
+            Fecha Gasto:
+          </label>
+          <DatePicker className="bg-slate-100 p-2 border-0" />
+        </div>
+
+        <input
+          type="submit"
+          className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+          value={"Registrar Gasto"}
+        />
+      </div>
+    </form>
+  );
 }
