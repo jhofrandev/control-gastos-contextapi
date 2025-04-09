23003598ccba8abcf084de286553c0699e18813f
Jhofran Briceño <jhofran.dev@outlook.com>
2025-04-09 10:50:39 -0500

    feat: add expense management and error message component
    Introduce a new ErrorMessege component to display error messages. Add functionality to manage expenses in the budget reducer, including generating unique IDs using uuid. Update the ExpenseForm to handle form submission and display errors. Add uuid and its types as dependencies.

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import DatePicker from "react-date-picker";
 
 import { DraftExpense, Value } from "../types";
@@ -6,6 +6,8 @@ import { categories } from "../data/categories";
 
 import "react-calendar/dist/Calendar.css";
 import "react-date-picker/dist/DatePicker.css";
+import ErrorMessege from "./ErrorMessege";
+import { useBudget } from "../hooks/useBudget";
 
 export default function ExpenseForm() {
   const [expense, setExpense] = useState<DraftExpense>({
@@ -14,6 +16,9 @@ export default function ExpenseForm() {
     category: "",
     date: new Date(),
   });
+  const [error, setError] = useState("");
+
+  const { dispatch } = useBudget();
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
@@ -33,11 +38,24 @@ export default function ExpenseForm() {
     });
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (Object.values(expense).includes("")) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+
+    dispatch({ type: "add-expense", payload: { expense } });
+  };
+
   return (
-    <form>
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
         Nuevo Gasto
       </legend>
+
+      {error && <ErrorMessege>{error}</ErrorMessege>}
 
       <div className="flex flex-col gap-2">
         <label htmlFor="expenseName" className="text-xl">
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,16 +1,30 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { DraftExpense, Expense } from "../types";
+
 export type BudgetActions =
   | { type: "add-budget"; payload: { budget: number } }
   | { type: "show-modal" }
-  | { type: "close-modal" };
+  | { type: "close-modal" }
+  | { type: "add-expense"; payload: { expense: DraftExpense } };
 
 export type BudgetState = {
   budget: number;
   modal: boolean;
+  expenses: Expense[];
 };
 
 export const initialState: BudgetState = {
   budget: 0,
   modal: false,
+  expenses: [],
+};
+
+const createExpense = (draftexpense: DraftExpense): Expense => {
+  return {
+    ...draftexpense,
+    id: uuidv4(),
+  };
 };
 
 export const budgetReducer = (
@@ -35,6 +49,15 @@ export const budgetReducer = (
     return {
       ...state,
       modal: false,
+    };
+  }
+
+  if (action.type === "add-expense") {
+    const expense = createExpense(action.payload.expense);
+
+    return {
+      ...state,
+      expenses: [...state.expenses, expense],
     };
   }
 
